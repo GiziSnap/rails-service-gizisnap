ec22351530c0261b250df36e7242701ca8b4f0b1
han <icyfarhan@gmail.com>
2025-06-11 16:51:54 +0700

    Completed the user after_create default avatar logic and render it on json for frontend to consume it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,13 @@ class UsersController < ApplicationController
 
   def index
     respond_to do |format|
-      format.json { render json: { userinformation: @current_user, userfoodshistory: @current_user.foods  } }
+      format.json { render json: { userinformation: @current_user, user_avatar: url_for(@current_user.avatar), userfoodshistory: @current_user.foods  } }
     end
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  has_one_attached :avatar
+
   has_many :sessions, dependent: :destroy
   has_many :foods, dependent: :destroy
 
@@ -8,4 +10,16 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true, length: { minimum: 6 }
   validates :email_address, uniqueness: true, presence: true
   validates :password, presence: true, length: { minimum: 6 }
+
+  after_create :create_user_default_avatar
+
+  private
+
+  def create_user_default_avatar
+    unless avatar.attached?
+      avatar.attach(io: File.open(Rails.root.join("app", "assets", "images", "default_avatar.png")),
+                    filename: "default_avatar.png",
+                    content_type: "image/png")
+    end
+  end
 end
